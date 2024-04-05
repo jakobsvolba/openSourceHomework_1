431bd32386c6d6fae00202078e945d1a49dc77e3
Jakob Svolba <jakob.svolba@gmail.com>
2024-04-05 10:21:40 +0200

    create main and function file

---
+++ functions.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Provides the functions for the main file
+
+Created on Fri Apr  5 10:20:01 2024
+
+@author: Jakob
+"""
+
---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Calculate the capacity factor and the fullload hours of an energy plant.
+
+Created on Fri Apr  5 09:31:28 2024
+
+@author: Jakob
+
+
+"""
+
+
+
+
