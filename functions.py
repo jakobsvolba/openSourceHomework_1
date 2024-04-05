7e15eef047fd833beb01746af49a1d3bea981f2b
Jakob Svolba <jakob.svolba@gmail.com>
2024-04-05 10:38:39 +0200

    write fullloadhours function

--- functions.py
+++ functions.py
@@ -20,8 +20,27 @@ def calc_capacity_factor(energy, power):
 
     Returns
     -------
-    capacity factor in %
+    cf in %
 
     '''
-    cf = energy / (power * 7860)
+    cf = (energy / (power * 7860))*100
     return cf
+
+def calc_fullloadhours(energy, power):
+    '''
+    calculates the fulloadhours of a powerplant with given parameters
+
+    Parameters
+    ----------
+    energy : 
+        yearly energy yield in kWh from a power plant
+    power : 
+        nominal power of power plant in kW
+
+    Returns
+    -------
+    fhours in hours
+    '''
+    fhours = energy / power
+    return fhours
+    
