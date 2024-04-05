644696911ee466625d9751bd6a6658b9e867b3b0
Jakob Svolba <jakob.svolba@gmail.com>
2024-04-05 10:59:20 +0200

    fix bug in capacity factor test function

--- test_functions.py
+++ test_functions.py
@@ -2,7 +2,7 @@
 import functions
 
 def test_calc_capacity_factor():
-    assert functions.calc_capacity_factor(5000, 5) == 19200
+    assert functions.calc_capacity_factor(19200, 5) == 0.48855
 
 def test_calc_fullloadhours():
     assert functions.calc_fullloadhours(5000, 5) == 1000
