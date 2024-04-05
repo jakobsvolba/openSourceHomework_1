cda7a6d7544ccff7ee68df8797cf34b0b7454619
Jakob Svolba <jakob.svolba@gmail.com>
2024-04-05 10:31:58 +0200

    write capacity factor function

--- functions.py
+++ functions.py
@@ -7,3 +7,21 @@ Created on Fri Apr  5 10:20:01 2024
 @author: Jakob
 """
 
+def calc_capacity_factor(energy, power):
+    '''
+    calculates the capacity factor of a powerplant with given parameters
+
+    Parameters
+    ----------
+    energy : 
+        yearly energy yield in kWh from a power plant
+    power : 
+        nominal power of power plant in kW
+
+    Returns
+    -------
+    capacity factor in %
+
+    '''
+    cf = energy / (power * 7860)
+    return cf
