fe394bac4d470805912b4f95d565c3373182dcd1
Jakob Svolba <jakob.svolba@gmail.com>
2024-04-05 11:34:41 +0200

    make test fail

--- test_functions.py
+++ test_functions.py
@@ -2,7 +2,7 @@
 import functions
 
 def test_calc_capacity_factor():
-    assert functions.calc_capacity_factor(3930, 5) == 10
+    assert functions.calc_capacity_factor(3930, 5) == 15
 
 def test_calc_fullloadhours():
     assert functions.calc_fullloadhours(5000, 5) == 1000
