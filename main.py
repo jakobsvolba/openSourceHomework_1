c4084aea379e1860998f1ea46e3a4660dfe08215
Jakob Svolba <jakob.svolba@gmail.com>
2024-04-05 11:10:44 +0200

    write main file, edit test function value

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ Created on Fri Apr  5 09:31:28 2024
 
 @author: Jakob
 
+Input Parameters:
+    energy: energy yield of powerplant in kWh
+    power: nominal (peak) power of powerplant in kW
 
 """
 
+import functions
 
+energy = 3930
+power = 5
+
+cf = functions.calc_capacity_factor(energy, power)
+fhours = functions.calc_fullloadhours(energy, power)
+
+print(f"The capacity factor of the power plant is {cf} %")
+print(f"The fullloadhours of the power plant is {fhours} hours")
 
 
--- test_functions.py
+++ test_functions.py
@@ -2,7 +2,7 @@
 import functions
 
 def test_calc_capacity_factor():
-    assert functions.calc_capacity_factor(19200, 5) == 0.48855
+    assert functions.calc_capacity_factor(3930, 5) == 10
 
 def test_calc_fullloadhours():
     assert functions.calc_fullloadhours(5000, 5) == 1000
